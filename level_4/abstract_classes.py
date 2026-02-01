b7097f55752e29c674445b249aea9900333153ff
UnknownStrange <princenyarkoedwin@gmail.com>
2026-02-01 15:09:40 +0000

    new level(level 4 added) its about abstracts

--- level_4/abstract_classes.py
+++ level_4/abstract_classes.py
@@ -1,33 +1,36 @@
-class BankAccount:
+from abc import ABC, abstractmethod
+
+class BankAccount(ABC):
     def __init__(self,balance):
-        self.__balance = balance
+        self._balance = balance
         
     @property
     def balance(self):
-        return self.__balance
+        return self._balance
     
-    def deposit(self, amount):
-        if amount > 0:
-            self.__balance += amount
-            
-    def withdraw(self,amount):
-        if amount <= self.__balance :
-            self.__balance -= amount
+    def  deposit(self,amount):
+        if amount > 0: 
+            self._balance += amount
+            print(f"An amount of {amount} has been deposited in your account successfully")
     
+    @abstractmethod
+    def withdraw(self, amount):
+        pass
+    @abstractmethod
     def account_detail(self):
-        print(f"Account balance is {self.__balance}")
+        pass
         
-        
-        
+    
+
 class SavingAccount(BankAccount):
     def __init__(self, balance, interest_rate, withdrawal_limit):
-        super().__init__(balance)
+        self.balance = balance
         self.interest_rate = interest_rate
         self.withdrawal_limit = withdrawal_limit
         self.withdraws_done = 0
         
     def withdraw(self,amount):
-        if self.withdraws_done < self.withdrawal_limit:
+        if self.withdraws_done < self.withdrawal_limit and amount <= self.balance:
             super().withdraw(amount)
             self.withdraws_done += 1
         else:
@@ -37,14 +40,3 @@ class SavingAccount(BankAccount):
         print(f"Account balance: {super().balance}")
         print(f"Interest rate: {self.interest_rate}")
         print(f"Withdrawals: {self.withdraws_done}/{self.withdrawal_limit}")
-        
-
-my_savings = SavingAccount(balance=1000, interest_rate=0.05, withdrawal_limit=3)
-my_savings.withdraw(400)
-my_savings.account_detail()
-
-my_savings.withdraw(400)
-
-my_savings.account_detail()
-
-my_savings.withdraw(10000)
